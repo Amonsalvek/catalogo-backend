5a5970d10b546857f9054b484cb924e9e2eaa5fd
Amonsalvek <60120312+Amonsalvek@users.noreply.github.com>
2025-06-17 13:40:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
 @app.route("/api/catalogo")
 def catalogo():
-    sheet = request.args.get("sheet", "accesos")  # accesos o catalogo
+    sheet = request.args.get("sheet", "accesos")
     url = f"https://script.google.com/macros/s/AKfycbzpaW4vFJ-vv_k3BfiSriZssmrwsmNmti747rKv5LXbz24c2sV7XRs6SC6fk9sTzle23g/exec?sheet={sheet}"
     response = requests.get(url)
     return jsonify(response.json())
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
