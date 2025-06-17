5fb7073269b37f7a3a76d19cac78fcf201bec617
Amonsalvek <60120312+Amonsalvek@users.noreply.github.com>
2025-06-17 14:12:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ def get_catalogo():
         print("EXCEPTION:", str(e))  # 🔍 IMPORTANTE
         return jsonify({"error": str(e)}), 500
 
+@app.route("/api/debug", methods=["GET"])
+def debug_sheet():
+    url = "https://script.google.com/macros/s/AKfycbzpaW4vFJ-vv_k3BfiSriZssmrwsmNmti747rKv5LXbz24c2sV7XRs6SC6fk9sTzle23g/exec?sheet=accesos"
+    r = requests.get(url)
+    print("STATUS:", r.status_code)
+    print("TEXT:", r.text[:500])
+    return r.text  # Devuelve texto plano para verificar qué llega
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
